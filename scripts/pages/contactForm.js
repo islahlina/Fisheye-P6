412f3ea0c1d93a8cfabd3bffc6fae38de55113c0
Melyna Kim <75linaa@gmail.com>
2022-08-13 09:56:16 +0200

    MAJ HTML : Index + photographers (validator) / MAJ JS : index

--- scripts/pages/contactForm.js
+++ scripts/pages/contactForm.js
@@ -2,7 +2,7 @@ function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
 }
-
+console.log(closeModal);
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,30 +1,13 @@
 async function getPhotographers() {
     // Penser à remplacer par les données récupérées dans le json
-    const photographers = [
-        {
-            "name": "Ma data test",
-            "id": 1,
-            "city": "Paris",
-            "country": "France",
-            "tagline": "Ceci est ma data test",
-            "price": 400,
-            "portrait": "account.png"
-        },
-        {
-            "name": "Autre data test",
-            "id": 2,
-            "city": "Londres",
-            "country": "UK",
-            "tagline": "Ceci est ma data test 2",
-            "price": 500,
-            "portrait": "account.png"
-        },
-    ]
+    const request = await fetch("data/photographers.json");
+    const data = await request.json();
+    const photographers = data.photographers;
     // et bien retourner le tableau photographers seulement une fois
     return ({
-        photographers: [...photographers, ...photographers, ...photographers]})
+        photographers
+    })
 }
-
 async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
 
@@ -34,7 +17,6 @@ async function displayData(photographers) {
         photographersSection.appendChild(userCardDOM);
     });
 };
-
 async function init() {
     // Récupère les datas des photographes
     const { photographers } = await getPhotographers();
@@ -42,3 +24,4 @@ async function init() {
 };
 
 init();
+
